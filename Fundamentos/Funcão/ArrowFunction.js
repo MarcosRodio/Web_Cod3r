821d2164c3b010c9bb4e1539223e95513d01ef80
Marcos Rodio <marcosrodio@gmail.com>
2022-04-28 17:13:31 -0300

    Add files via upload

---
+++ Fundamentos/Funcão/ArrowFunction.js
@@ -0,0 +1,33 @@
+let dobro = function (a) {
+    return 2 * a;
+}
+
+console.log(dobro(4));
+
+dobro = (a) => {return "O número é: " + 3 * a};
+
+
+console.log(dobro(4));
+
+dobro = (a) => `O Numero é: ${4 * a}`;
+
+console.log(dobro(4));
+
+let ola = () => console.log("Olá...");
+
+ola ();
+
+ola = ()=> {return "Teste do OOOOLLLÀÀÀÀ"}; 
+
+console.log(ola());
+
+let tabuada = (a) => {
+    
+    console.log(`\nTabuada do ${a}...\n`)
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${a} x ${i} = ${a*i}` );
+       }
+
+    }
+
+tabuada(1);
